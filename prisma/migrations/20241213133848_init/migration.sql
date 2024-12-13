859f82a76ef5992023eb0030ed8b95728142542a
oleg-khromov <dev.oleg.khromov@gmail.com>
2024-12-13 14:40:12 +0100

    changed init migration

--- prisma/migrations/20241213133848_init/migration.sql
+++ prisma/migrations/20241213133848_init/migration.sql
@@ -1,5 +1,23 @@
 -- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
 CREATE TYPE "Status" AS ENUM ('Pending', 'InProgress', 'Resolved');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "phoneNumber" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Ticket" (
@@ -60,6 +78,9 @@ CREATE TABLE "PasswordResetToken" (
 
     CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Category_title_key" ON "Category"("title");
